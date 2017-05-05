4ce238d38f2bb90394f66b88da29f6be2ebf12c9
takumi314 <isospin_symmetry@me.com>
2017-05-06 01:58:04 +0900

    implement a TabBarViewController with NavigationController

--- Sample-API-Layer-Demo/Views/MainTabBarController.swift
+++ Sample-API-Layer-Demo/Views/MainTabBarController.swift
@@ -10,5 +10,30 @@ import UIKit
 
 class MainTabBarController: UITabBarController {
 
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+
+        var controllers: [UIViewController] = []
+
+        let areaViewController = AreaListsViewController()  // ViewController = Name of your controller
+        let navigation = UINavigationController()
+        navigation.tabBarItem = UITabBarItem(tabBarSystemItem: .contacts, tag: 0)
+        navigation.viewControllers = [areaViewController]
+        controllers.append(navigation)
+
+        let secondViewController = UIViewController()
+        secondViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        controllers.append(secondViewController)
+
+        let thirdViewController = UIViewController()
+        thirdViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .featured, tag: 2)
+        controllers.append(thirdViewController)
+
+        self.setViewControllers(controllers, animated: false)
+
+        AppDelegate().window = UIWindow(frame: UIScreen.main.bounds)
+        AppDelegate().window?.rootViewController = self
+        AppDelegate().window?.makeKeyAndVisible()
+    }
 
 }
