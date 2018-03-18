1db26819654a3e3121f137d94c8f987fa0891c49
takumi314 <isospin_symmetry@me.com>
2018-03-18 23:58:11 +0900

    make api speciafications

--- Sample-API-Layer-Demo/API/Api+Prefactures.swift
+++ Sample-API-Layer-Demo/API/Api+Prefactures.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+import Moya
+
+extension Gnavi {
+
+    struct GetPrefactures: GnaviApiTargetType {
+        typealias Response = AreaLMasters
+
+        /// The path to be appended to `baseURL` to form the full `URL`.
+        public var path: String {
+            return "master/PrefSearchAPI/20150630/"
+        }
+        /// The HTTP method used in the request.
+        public var method: Moya.Method {
+            return .get
+        }
+        /// The type of HTTP task to be performed.
+        public var task: Moya.Task {
+            let parameters = ["keyid": APIConfiguration.accessKey,
+                              "format": "json",
+                              "lang": "ja"] as [String : Any]
+            return .requestParameters(parameters: parameters, encoding: URLEncoding.default)
+        }
+    }
+
+}
--- Sample-API-Layer-Demo/API/Api+Restraunt.swift
+++ Sample-API-Layer-Demo/API/Api+Restraunt.swift
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+import Moya
+
+extension Gnavi {
+
+    struct GetRestraunts: GnaviApiTargetType {
+        typealias Response = GnaviResults
+
+        /// The path to be appended to `baseURL` to form the full `URL`.
+        public var path: String {
+            return "RestSearchAPI/20150630/"
+        }
+        /// The HTTP method used in the request.
+        public var method: Moya.Method {
+            return .get
+        }
+        /// The type of HTTP task to be performed.
+        public var task: Moya.Task {
+            let parameters = ["keyid": "\(APIConfiguration.accessKey)",
+                "format": "json",
+                "callback": "",
+                "pref": prefCode,
+                "areacode_l": "",
+                "hit_per_page": ONCE_READ_COUNT,
+                "offset_page": page] as [String : Any]
+            return .requestParameters(parameters: parameters, encoding: URLEncoding.default)
+        }
+
+        private let page: Int
+        private let prefCode: String
+        private let perPage: Int
+
+        init(page: Int, prefCode: String, perPage: Int = ONCE_READ_COUNT) {
+            self.page   = page
+            self.prefCode = prefCode
+            self.perPage = perPage
+        }
+    }
+    
+}
+
+
+
