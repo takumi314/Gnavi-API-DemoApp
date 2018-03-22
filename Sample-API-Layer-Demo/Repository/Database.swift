270d92c30cfbe7787845bf59cafca898b0eba87e
takumi314 <isospin_symmetry@me.com>
2018-03-22 13:12:40 +0900

    define protocols for Any Database

--- Sample-API-Layer-Demo/Repository/Database.swift
+++ Sample-API-Layer-Demo/Repository/Database.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+
+typealias Database = ReadableDatabase & WritableDatabase
+typealias Query = (() -> Bool)
+
+protocol Model { }
+
+protocol ReadableDatabase {
+    func loadAllObjects<T: Model>() -> [T]
+    func loadObjects<T: Model>(matching query: Query) -> [T]
+    func loadObject<T: Model>(withID id: String) -> T?
+}
+
+protocol WritableDatabase {
+    func save<T: Model>(_ object: T) -> Bool
+    func update<T: Model>(_ object: T) -> Bool
+    func delete(withID id: String) -> Bool
+}
+
+
