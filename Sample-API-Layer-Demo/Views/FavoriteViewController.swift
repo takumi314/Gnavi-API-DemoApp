deeba0e2873cd1224312e551d81b5ec233bb13c0
takumi314 <isospin_symmetry@me.com>
2018-03-25 22:08:00 +0900

    make new ViewController

---
+++ Sample-API-Layer-Demo/Views/FavoriteViewController.swift
@@ -0,0 +1,34 @@
+//
+//  FavoriteViewController.swift
+//  Sample-API-Layer-Demo
+//
+//  Created by NishiokaKohei on 2018/03/25.
+//  Copyright © 2018年 Kohey. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteViewController: UIViewController {
+
+
+    // MARK: - Initialier
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Life cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
