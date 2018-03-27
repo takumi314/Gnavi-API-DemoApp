b1f88414872493edb8b6fe8462e8aa14c175839b
takumi314 <isospin_symmetry@me.com>
2018-03-28 01:52:30 +0900

    make it for showing favorite's list

--- Sample-API-Layer-Demo/Views/FavoriteViewController.swift
+++ Sample-API-Layer-Demo/Views/FavoriteViewController.swift
@@ -11,9 +11,15 @@ import UIKit
 class FavoriteViewController: UIViewController {
 
 
+    private var dataSource: FavoriteTableViewDataSource
+    private var tableView: UITableView?
+
     // MARK: - Initialier
 
     init() {
+        let manager = RealmManager<FavoriteModel>()
+        let favorites = manager.loadAllObjects()
+        dataSource = FavoriteTableViewDataSource(favorites)
         super.init(nibName: nil, bundle: nil)
     }
 
@@ -25,10 +31,52 @@ class FavoriteViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Favorite"
+        tableView = registerNib(for: UITableView(frame: self.view.frame, style: .plain))
+        view.addSubview(tableView!)
+        tableView?.dataSource = dataSource
+        tableView?.delegate = self
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        let manager = RealmManager<FavoriteModel>()
+        let favorites = manager.loadAllObjects()
+        dataSource = FavoriteTableViewDataSource(favorites)
+        tableView?.dataSource = dataSource
+        tableView?.delegate = self
+        tableView?.reloadData()
+        super.viewWillAppear(animated)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 
+    private func registerNib(for tableView: UITableView?) -> UITableView {
+        guard let tableView = tableView
+            else { return UITableView() }
+        let nib = UINib(nibName: master(of: .restraunt), bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "rest")
+        return tableView
+    }
+
+    private func master(of type: APIMasterType) -> String {
+        switch type {
+        case .prefacture:
+            return "PrefactureTableViewCell"
+        case .restraunt:
+            return "RestrauntTableViewCell"
+        }
+    }
+
+
+
 }
+
+extension FavoriteViewController: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80.0
+    }
+}
+
