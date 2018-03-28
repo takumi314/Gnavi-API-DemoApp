af84aad0b26ac9d3507ad07a1d2e7cce9c58ab24
takumi314 <isospin_symmetry@me.com>
2018-03-29 01:39:45 +0900

    make new coordinator class for the first tab

---
+++ Sample-API-Layer-Demo/Coodinators/FirstTabCoordinator.swift
@@ -0,0 +1,13 @@
+//
+//  FirstTabCoordinator.swift
+//  Sample-API-Layer-Demo
+//
+//  Created by NishiokaKohei on 2018/03/29.
+//  Copyright © 2018年 Kohey. All rights reserved.
+//
+
+import UIKit
+
+class FirstTabCoordinator {
+}
+
