f862abab569796229dd823390ba711a3454dc63e
takumi314 <isospin_symmetry@me.com>
2018-03-28 22:47:01 +0900

    make new class

---
+++ Sample-API-Layer-Demo/Coodinators/ApplicationCoordinator.swift
@@ -0,0 +1,12 @@
+//
+//  ApplicationCoordinator.swift
+//  Sample-API-Layer-Demo
+//
+//  Created by NishiokaKohei on 2018/03/28.
+//  Copyright © 2018年 Kohey. All rights reserved.
+//
+
+import UIKit
+
+class ApplicationCoordinator {
+}
