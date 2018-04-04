c54b3f5d28a15233449599627d49d9287b465411
takumi314 <isospin_symmetry@me.com>
2018-04-04 23:58:30 +0900

    make new coordinator for Favourite

---
+++ Sample-API-Layer-Demo/Coodinators/FavoriteCoordinator.swift
@@ -0,0 +1,9 @@
+//
+//  FavoriteCoordinator.swift
+//  Sample-API-Layer-Demo
+//
+//  Created by NishiokaKohei on 2018/04/04.
+//  Copyright © 2018年 Kohey. All rights reserved.
+//
+
+import Foundation
